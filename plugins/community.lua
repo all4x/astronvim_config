245a18cc0f50cae7f62d5b475de1e1371b2baf49
alex <alexfullstackdevops@gmail.com>
2024-02-15 06:03:21 -0300

    first commit

---
+++ plugins/community.lua
@@ -0,0 +1,14 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.pack.typescript"},
+    { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+}
