3d035b6cd2d35f132422e9beb04c7da1198658f5
Alex Rodrigues <54729143+all4x@users.noreply.github.com>
2024-02-19 03:39:37 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "monokai-pro-octagon",
+  colorscheme = "nordfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
